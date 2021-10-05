b4c25fff0a95b019750f5a0f8eb86150809d758c
katrina <katrina.nov13@gmail.com>
2021-10-05 20:17:36 +0300

    Negative Connotation

--- scripts/kyu7.js
+++ scripts/kyu7.js
@@ -10,3 +10,17 @@ function decodePass( passArr, bin ){
   const password = passArr.find(word => word == bin.split(' ').map(char => String.fromCharCode(parseInt(char, 2))).join(''));
   return password ? password : false;  
 }
+
+// Negative Connotation
+/* You will be given a string with sets of characters, (i.e. words), seperated by between one and three spaces (inclusive).
+Looking at the first letter of each word (case insensitive-"A" and "a" should be treated the same), you need to determine whether it falls into the positive/first half of the alphabet ("a"-"m") or the negative/second half ("n"-"z").
+Return True/true if there are more (or equal) positive words than negative words, False/false otherwise.*/
+const str = "  ikx  o i  l z     vi      t  q      gj";
+
+const pos = str.split(' ').filter(word => word.charAt(0) < 'n');
+function connotation(str) {
+  const strToArr = str.split(' ').filter(word => word !== '');
+  const pos = strToArr.filter(word => word.charAt(0).toLowerCase() < 'n');
+  return 2 * pos.length >= strToArr.length;
+}
+console.log(connotation(str));

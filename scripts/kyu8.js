0ad9bd67001ce4474873d30b898e7d03adc115e3
katrina <katrina.nov13@gmail.com>
2021-10-06 21:45:58 +0300

    Multiplication table for number

---
+++ scripts/kyu8.js
@@ -0,0 +1,10 @@
+//Multiplication table for number
+var number = 5;
+function multiTable(number) {
+  // good luck
+  let str = `1 * ${number} = ${number}`;  
+  for (let i = 2; i < 11; i++) {   
+    str += `\n${i} * ${number} = ${i*number}`;
+  }
+  return str;
+}

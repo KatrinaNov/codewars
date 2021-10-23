506ed1a1f08512a42e8becc3b93a917822c74442
katrina <katrina.nov13@gmail.com>
2021-10-23 18:08:12 +0300

    Pairs of integers from m to n

--- scripts/kyu7.js
+++ scripts/kyu7.js
@@ -68,3 +68,15 @@ var number = function(busStops){
   }
   return people;
 }
+
+// Pairs of integers from m to n
+function generatePairs(m, n) {
+   let arr = [];
+  for (let i=m; i <= n; i++) {
+    for (let j = i; j <= n; j++) {
+      arr.push([i, j]);
+    }
+  }
+  return arr;
+}
+console.log(generatePairs(2, 4));
